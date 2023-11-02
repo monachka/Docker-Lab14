9b9f1d9696b685fda088a8bdeef7c28ee89f7f21
monachka <142250148+monachka@users.noreply.github.com>
2023-11-02 17:19:55 +0000

    Update User.java

--- src/main/java/com/dame/uasz/sbcar3/domain/User.java
+++ src/main/java/com/dame/uasz/sbcar3/domain/User.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "my_user")
+//@Table(name = "my_user")
 public class User extends BaseEntity{
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
