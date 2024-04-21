66532c36c2e39d9d232eba1d90acb110f64c7133
monachka <142250148+monachka@users.noreply.github.com>
2024-04-21 12:45:28 +0000

    Update CorsConfig.java

--- src/main/java/com/dame/uasz/sbcar3/CorsConfig.java
+++ src/main/java/com/dame/uasz/sbcar3/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://my-car-app2.surge.sh")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
